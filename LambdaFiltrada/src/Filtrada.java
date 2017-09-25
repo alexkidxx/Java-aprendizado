548001383baaa9467721f492390c7f7147043d6d
Alexsandro Martins <alex.kidxx@gmail.com>
2017-09-25 13:38:46 -0300

    Exemplo de utilização de expressões lambda

---
+++ LambdaFiltrada/src/Filtrada.java
@@ -0,0 +1,28 @@
+		
+		
+		
+		import java.util.Collection;
+		import java.util.List;
+		import java.util.stream.Collectors;
+public class Filtrada {
+	 private static List<Pessoa> listPessoas;
+	public static void main(String[] args) {
+
+
+			
+			
+			  System.out.println("Pessoas com mais de 30 anos:");
+			 
+			List<Pessoa> maioresTrinta = listPessoas.stream().filter(p -> p.getIdade() > 30).collect(Collectors.toList());
+			  maioresTrinta.forEach(p -> System.out.println(p.getNome()));
+			       
+			  System.out.println("Pessoas que o nome iniciam com E:");
+			  List<Pessoa> nomesIniciadosE = listPessoas.stream().filter(p -> ((String) p.getNome()).startsWith("E")).collect(Collectors.toList());
+			  nomesIniciadosE.forEach(p -> System.out.println(p.getNome()));	
+
+	
+
+
+	}
+
+}
---
+++ LambdaFiltrada/src/Pessoa.java
@@ -0,0 +1,14 @@
+
+public class Pessoa {
+
+	public int getIdade() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object getNome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
