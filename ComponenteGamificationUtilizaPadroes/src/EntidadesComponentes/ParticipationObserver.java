6a5b2a214361735b5a6cfd2b615948ec603817ed
Alexsandro Martins Dias <alexkidxx@users.noreply.github.com>
2017-10-17 16:47:23 -0200

    Observer de participação
    Observer que é notificado a cada novo achievement adicionado e analisa se o usuário é digno de um badge de pontuação de participação

---
+++ ComponenteGamificationUtilizaPadroes/src/EntidadesComponentes/ParticipationObserver.java
@@ -0,0 +1,28 @@
+package EntidadesComponentes;
+
+import interfaces.AchievementObserver;
+
+public class ParticipationObserver implements AchievementObserver {
+
+	@Override
+	public void achievementUpdate(String user, Achievement a) {
+		if(a instanceof Points && a.getNome().equals("PARTICIPATION")){
+			verificaPontuaçãoParticipation(user);	
+				
+			}
+			
+
+	}
+	
+public void verificaPontuaçãoParticipation(String user){
+		
+		MemoryAchievementStorage memo = (MemoryAchievementStorage) AchievementStorageFactory.getAchievementStorage();
+		int pontosParticipation = ((Points) memo.getAchievement(user, "PARTICIPATION")).getValor();
+		if(pontosParticipation >= 100)
+			
+		
+			memo.addAchievement(user, new Badge("PART OF THE COMMUNITY"));
+	}
+
+
+}
