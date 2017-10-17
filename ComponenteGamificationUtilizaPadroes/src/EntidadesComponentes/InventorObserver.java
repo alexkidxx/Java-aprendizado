263c75af2cccc4293f3bca24d7fd31dec55027c4
Alexsandro Martins Dias <alexkidxx@users.noreply.github.com>
2017-10-17 16:44:50 -0200

    Observer para badge inventor
    Observer que é notificado todas as vezes que um uma chamada de Points é feita e verifica a quantidade de determinado tipo de ponto para criar novo badge ao chegar a 100 pontos

---
+++ ComponenteGamificationUtilizaPadroes/src/EntidadesComponentes/InventorObserver.java
@@ -0,0 +1,28 @@
+package EntidadesComponentes;
+
+import interfaces.AchievementObserver;
+
+public class InventorObserver implements AchievementObserver {
+
+
+	
+	
+	@Override
+	public void achievementUpdate(String user, Achievement a) {
+		if(a instanceof Points && a.getNome().equals("CREATION")){
+		verificaPontuaçãoInventor(user);	
+			
+		}
+		
+	}
+	
+	
+	public void verificaPontuaçãoInventor(String user){
+		
+		MemoryAchievementStorage memo = (MemoryAchievementStorage) AchievementStorageFactory.getAchievementStorage();
+		int pontosCreation = ((Points) memo.getAchievement(user, "CREATION")).getValor();
+		if(pontosCreation >= 100)
+			memo.addAchievement(user, new Badge("INVENTOR"));
+	}
+
+}
