54554124110a0efa3eda4b4e9d723596c1693cb3
Alexsandro Martins Dias <alexkidxx@users.noreply.github.com>
2017-10-17 16:34:02 -0200

    Final version

--- ComponenteGamificationUtilizaPadroes/src/interfaces/AchievementStorage.java
+++ ComponenteGamificationUtilizaPadroes/src/interfaces/AchievementStorage.java
@@ -3,11 +3,19 @@ package interfaces;
 import java.util.List;
 
 import EntidadesComponentes.Achievement;
+import EntidadesComponentes.Usuario;
 
 public interface AchievementStorage {
 	
 	void addAchievement(String user,Achievement a);
+	
+
 	List<Achievement>getAchievements(String user);
+	
 	Achievement getAchievement(String user, String achievementName);
+	boolean possuiEssaConquista(String name, String user);
+	
+	Usuario getUser(String user);
+	
 
 }
