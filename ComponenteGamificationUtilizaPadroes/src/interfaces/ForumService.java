8677de62398baaf5660685e85769bab38b569774
Alexsandro Martins Dias <alexkidxx@users.noreply.github.com>
2017-10-17 16:34:27 -0200

    Update ForumService.java

--- ComponenteGamificationUtilizaPadroes/src/interfaces/ForumService.java
+++ ComponenteGamificationUtilizaPadroes/src/interfaces/ForumService.java
@@ -2,4 +2,11 @@ package interfaces;
 
 public interface ForumService {
 
+	void addTopic(String user,String topic);
+	void addComment(String user,String topic, String topicUser);
+	void likeTopic(String user, String topic,String topicUser);
+	void likeComment(String user, String topic, String comment, String commentUser);
+	
+	
+	
 }
